d343ec78a15cb1d89bc74b68294495a23011c463
danzeph <danzeph00@gmail.com>
2025-12-19 23:35:34 +0000

    Initialize alx_book_store database and tables

---
+++ alx_book_store.sql
@@ -0,0 +1,47 @@
+DROP DATABASE IF EXISTS alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS order_details;
+
+CREATE TABLE authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE books(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id)
+);
+
+
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) UNIQUE,
+    address TEXT
+    );
+    
+CREATE TABLE orders(
+	order_id INT Primary Key AUTO_INCREMENT,
+	customer_id INT,
+	order_date DATE,
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_details(
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY(order_id) REFERENCES orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES books(book_id)
+);
