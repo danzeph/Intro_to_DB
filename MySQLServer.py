25f3f14e0b41adde62eff423890b55524e8568ba
danzeph <danzeph00@gmail.com>
2025-12-19 23:52:11 +0000

    Refine error handling in create_database function
    Updated exception handling to use specific MySQL connector error.

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -19,7 +18,7 @@ def create_database():
 
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
