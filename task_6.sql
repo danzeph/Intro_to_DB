c8a1b09e59cfe4d7bc28686fe26448066dec2d9e
danzeph <danzeph00@gmail.com>
2025-12-20 10:13:19 +0000

    Create task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,12 @@
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address) 
+ VALUES
+ (2, 'Blessing Malik', 'bmalik@sandtech.com', '123 Happiness Ave.'),
+ (3, 'Obed Ehoneah', 'eobed@sandtech', '123 Happiness Ave.'),
+ (4, 'Nehemial Kamolu','nkamolu@sandtech.com', '123 Happiness Ave.'),
+ (5, 'dan amaz', 'danamaz@sandtech.com', '123 Happiness Ave.'),
+ (6, 'Cole Palmer', 'cpalmer@sandtech.com', '123 Happiness Ave.');
+
+
+SELECT * FROM customers;
