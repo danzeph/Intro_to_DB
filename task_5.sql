7e21b492dc6301ef7c2f9352806366b7be2cba83
danzeph <danzeph00@gmail.com>
2025-12-20 09:56:04 +0000

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,12 +1,9 @@
+-- Select the database
 USE alx_book_store;
 
-INSERT INTO customers VALUES(
-    'customer_id' = 1,
-    'customer_name' = 'Cole Baidoo',
-    'email' = 'cbaidoo@sandtech.com',
-    'address' = '123 Happiness Ave.'
-    );
+-- Insert a single record using standard syntax
+INSERT INTO customers (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
 
+-- Verify the insertion
 SELECT * FROM customers;
-
-
