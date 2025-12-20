aac7f19aee8bef182f490cf5d928831f83ce1290
danzeph <danzeph00@gmail.com>
2025-12-20 09:42:15 +0000

    Fix address formatting in customer insert statement

--- task_5.sql
+++ task_5.sql
@@ -4,7 +4,7 @@ INSERT INTO customers VALUES(
     'customer_id' = 1,
     'customer_name' = 'Cole Baidoo',
     'email' = 'cbaidoo@sandtech.com',
-    'address' = '123 Happiness Ave'
+    'address' = '123 Happiness Ave.'
     );
 
 SELECT * FROM customers;
