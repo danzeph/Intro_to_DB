f07da6fbb5da97cbd55eceff666d888775f81d5e
danzeph <danzeph00@gmail.com>
2025-12-20 10:19:49 +0000

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -2,11 +2,11 @@ USE alx_book_store;
 
 INSERT INTO customers (customer_id, customer_name, email, address) 
  VALUES
- (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
- (3, 'Obed Ehoneah', 'eobed@sandtech', '125 Happiness Ave.'),
- (4, 'Nehemial Kamolu','nkamolu@sandtech.com', '126 Happiness Ave.'),
- (5, 'dan amaz', 'danamaz@sandtech.com', '127 Happiness Ave.'),
- (6, 'Cole Palmer', 'cpalmer@sandtech.com', '128 Happiness Ave.');
+ (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+ (3, 'Obed Ehoneah', 'eobed@sandtech', '125 Happiness  Ave.'),
+ (4, 'Nehemial Kamolu','nkamolu@sandtech.com', '126 Happiness  Ave.'),
+ (5, 'dan amaz', 'danamaz@sandtech.com', '127 Happiness  Ave.'),
+ (6, 'Cole Palmer', 'cpalmer@sandtech.com', '128 Happiness  Ave.');
 
 
 SELECT * FROM customers;
